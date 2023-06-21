82545c5fe21465d11a5ecf2acabd6ca6fff77e9c
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2023-06-21 14:52:24 +0200

    Added Eigen to source dependencies

--- 01_st_raphael_sim/cmake/FetchRTAC.cmake
+++ 01_st_raphael_sim/cmake/FetchRTAC.cmake
@@ -31,8 +31,19 @@ function(FetchRTAC)
     message(STATUS "RTAC_OPTIX_VERSION      : ${RTAC_OPTIX_VERSION}")
     message(STATUS "RTAC_SIMULATION_VERSION : ${RTAC_SIMULATION_VERSION}")
 
+    include(FetchContent)
+
+    find_package(Eigen 3.4 QUIET)
+    # Getting Eigen 3.4 if no suitable version was found
+    if(NOT TARGET Eigen3::Eigen)
+        FetchContent_Declare(Eigen34
+            GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
+            GIT_TAG        3.4
+        )
+        FetchContent_MakeAvailable(Eigen34)
+    endif()
+
     if(NOT TARGET rtac_base)
-        include(FetchContent)
         FetchContent_Declare(rtac_base
             GIT_REPOSITORY https://github.com/ENSTABretagneRobotics/rtac_base.git
             GIT_TAG        ${RTAC_BASE_VERSION}
@@ -41,7 +52,6 @@ function(FetchRTAC)
     endif()
     
     if(NOT TARGET rtac_display)
-        include(FetchContent)
         FetchContent_Declare(rtac_display
             GIT_REPOSITORY https://github.com/ENSTABretagneRobotics/rtac_display.git
             GIT_TAG        ${RTAC_DISPLAY_VERSION}
@@ -50,7 +60,6 @@ function(FetchRTAC)
     endif()
     
     if(NOT TARGET rtac_optix)
-        include(FetchContent)
         FetchContent_Declare(rtac_optix
             GIT_REPOSITORY https://github.com/ENSTABretagneRobotics/rtac_optix.git
             GIT_TAG        ${RTAC_OPTIX_VERSION}
@@ -59,7 +68,6 @@ function(FetchRTAC)
     endif()
     
     if(NOT TARGET rtac_simulation)
-        include(FetchContent)
         FetchContent_Declare(rtac_simulation
             GIT_REPOSITORY https://github.com/ENSTABretagneRobotics/rtac_simulation.git
             GIT_TAG        ${RTAC_SIMULATION_VERSION}
